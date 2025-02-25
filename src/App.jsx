f88be93c8d2fc78d8f6f78076f8f13694f70197d
Avril Donovan Gonzales <88221381+adgonzales18@users.noreply.github.com>
2025-02-26 05:53:51 +0800

    changed to Hashrouter instead of BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useEffect, useState } from 'react'
-import './App.css'
-import Header from './pages/Header'
-import Banner from './pages/Banner'
-import TechnologyStack from './pages/TechnologyStack'
-import Portfolio from './pages/Portfolio'
-import Footer from './pages/Footer'
-import BackToTopBtn from './components/BackToTopBtn'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import './App.css';
+import Header from './pages/Header';
+import Banner from './pages/Banner';
+import TechnologyStack from './pages/TechnologyStack';
+import Portfolio from './pages/Portfolio';
+import Footer from './pages/Footer';
+import BackToTopBtn from './components/BackToTopBtn';
 import Contact from './pages/Contact';
 
 function App() {
