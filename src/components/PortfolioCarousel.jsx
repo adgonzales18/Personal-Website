5c4edb29e0ea64c90e7ecfb444fb5d8b72dbcc0d
Avril Donovan Gonzales <88221381+adgonzales18@users.noreply.github.com>
2025-03-06 04:08:28 +0800

    fix the techstack margins

--- src/components/PortfolioCarousel.jsx
+++ src/components/PortfolioCarousel.jsx
@@ -36,22 +36,35 @@ function PortfolioCarousel() {
                     <a href='' className="project-link" ><ion-icon name="link-outline"></ion-icon></a>
                 </div>
             </div>
+            <div className="carousel-caption-small">
+              <h2>FLIXIFY</h2>
+              <p>A streaming website.</p>
+              <p>TECH STACK: React, JavaScript, Bootstrap</p>
+              <a href='' className="project-link" ><ion-icon name="link-outline"></ion-icon></a>
+            </div>
       </Carousel.Item>
       <Carousel.Item
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}>
             <div className={`carousel-item-wrapper ${hovered ? 'hovered' : ''}`}>
+              <div>
                 <video autoPlay muted loop className='carousel-video'>
                 <source src={ToDoVideo} type="video/mp4"/>
                 </video>
+              </div>
                 <div className="carousel-caption-wrapper">
                     <h2>TO-DO APP</h2>
                     <p>Basic To-Do App.</p>
                     <p>TECH STACK: React, JavaScript, CSS Styling</p>
                     <a href="/" className="project-link" ><ion-icon name="link-outline"></ion-icon></a>
-                
+                </div>
             </div>
-        </div>
+            <div className="carousel-caption-small">
+              <h2>TO-DO APP</h2>
+              <p>Basic To-Do App.</p>
+              <p>TECH STACK: React, JavaScript, CSS Styling</p>
+              <a href="/" className="project-link" ><ion-icon name="link-outline"></ion-icon></a>
+            </div>
       </Carousel.Item>
     </Carousel>
   );
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
             <p>© 2024 Agile DevGrit, Inc.</p>
             <div className="social-media-links">
                 <a class="text-body-secondary" href="https://github.com/adgonzales18"><i class="fa-brands footer-icon fa-github" aria-hidden="true"></i></a>
-                <a class="text-body-secondary" href="https://github.com/adgonzales18"><i class="fa-brands footer-icon fa-linkedin" aria-hidden="true"></i></a>
+                <a class="text-body-secondary" href="https://www.linkedin.com/in/avrilgonzales/"><i class="fa-brands footer-icon fa-linkedin" aria-hidden="true"></i></a>
                 <a class="text-body-secondary" href="https://www.instagram.com/avrilgonzales"><i class="fa-brands footer-icon fa-instagram" aria-hidden="true"></i></a>
             </div>
         </div>
