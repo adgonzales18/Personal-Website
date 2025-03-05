b78e7caa717bc7b6cac77ce8533e1bcd7a4abc9e
Avril Donovan Gonzales <88221381+adgonzales18@users.noreply.github.com>
2025-03-06 00:36:00 +0800

    unfixed header below 640px

--- src/components/TechStackCard.jsx
+++ src/components/TechStackCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function TechStackCard({icon, techName}) {
   return (
-    <div className="col-lg-2 col-md-4 col-sm-6">
-        <div className='techstack-card'>
+    <div className="col-lg-2 col-md-4 col-sm-4 col-6">
+        <div className="techstack-card">
             {icon}
             <h2>{techName}</h2>
         </div>
